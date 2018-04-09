a9265df7d7d47ed5ebc8d5ff0f8d5b45252500d1
Jane Asawa <asawajane@gmail.com>
2018-04-09 00:19:01 -0700

    added firebase and changed some variable names

---
+++ assets/javascript/app.js
@@ -0,0 +1,39 @@
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDoVSCDI4teOKVAId_upqDVE1ohm1lmVrI",
+    authDomain: "westwood-train-station.firebaseapp.com",
+    databaseURL: "https://westwood-train-station.firebaseio.com",
+    projectId: "westwood-train-station",
+    storageBucket: "westwood-train-station.appspot.com",
+    messagingSenderId: "597462120364"
+  };
+  firebase.initializeApp(config);
+
+// variable that references the database
+  var database = firebase.database();
+
+// initial variables
+
+
+// When button is clicked then the user's train input is transferred to the variables
+$("#newTrain").on("click", function(event) {
+    event.preventDefault();
+    var trainName = $("#new-train-input").val().trim();
+    var destination = $("#new-destination-input").val().trim();
+    var firstTime = $("#new-1st-train-time-input").val().trim();
+    var frequency = $("#new-frequency-input").val().trim();
+
+    // Make local "temporary" object to hold train data
+    var train = {
+        trainName: trainName,
+        destination: destination,
+        firstTime: firstTime,
+        frequency: frequency
+    };
+
+    // upload train data to firebase
+    database.ref().push(train);
+})
+
+
