831bd655b350a1a2db65bf8399c0c3cff7554c9d
Jane Asawa <asawajane@gmail.com>
2018-04-09 12:28:10 -0700

    added firbase event listener which will fire when new train added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -32,16 +32,38 @@ $("#addTrainButton").on("click", function(event) {
         frequency: frequency
     };
 
-    console.log(train);
+    console.log("This is the info from the input " + train);
 
     // Upload train data to firebase
     database.ref().push(train);
 
     // Clear all text input boxes
-    $("#new-train-input").val("");
-    $("#new-destination-input").val("");
-    $("#new-1st-train-time-input").val("");
-    $("#new-frequency-input").val("");
+    $("#new-train-input").val(" ");
+    $("#new-destination-input").val(" ");
+    $("#new-1st-train-time-input").val(" ");
+    $("#new-frequency-input").val(" ");
 });
+
+// Make a Firebase event listener to register when any user adds a train to the database
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+    console.log("This is the info from firebase " + childSnapshot.val());
+
+    // store info from firebase into variables
+    var train = childSnapshot.val().trainName;
+    var trainDestination = childSnapdot.val().destination;
+    var trainFirstTime = childSnapshot.val().firstTime;
+    var trainFrequency = childSnapshot.val().frequency;
+
+    // calculate Next Arrival for Current Train Schedule
+    
+
+
+    // calculate Minutes Away for Current Train Schedule
+
+
+})
+
+
+// Displays a new row in the "Current Train Schedule" when any user adds a train
 
 
